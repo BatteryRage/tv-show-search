26c5698390d743c01911a1cd05d0facb4257aaed
Andrew Fayed <andrew.fayed@gmail.com>
2022-03-18 18:20:04 -0600

    Created card layout v1

--- createElements.js
+++ createElements.js
@@ -1,21 +1,37 @@
 const makeElements = (show) => {
-  const showTitle = document.createElement("h4");
-  const showNetwork = document.createElement("h5");
+  const showTitle = document.createElement("h5");
+  const showNetwork = document.createElement("h6");
   const showSummary = document.createElement("p");
   const showImg = document.createElement("img");
   const div = document.querySelector("#searchResults");
 
+  // Define all parts of the card
+  const card = document.createElement("div");
+  const cardBody = document.createElement("div");
+  card.classList.add("card");
+  card.classList.add("bg-light");
+  card.classList.add("col-md-5");
+  card.classList.add("m-1");
+  cardBody.classList.add("card-body");
+  showTitle.classList.add("card-title");
+  showNetwork.classList.add("card-subtitle");
+  showSummary.classList.add("card-text");
+  showImg.classList.add("justify-self-center")
+
+  // Add content to elements
   showTitle.append(show.title);
   showNetwork.append("Network: " + show.network);
   showSummary.innerHTML = show.summary;
   imagestr = `<img src="${show.image}" >`;
   showImg.src = show.image;
-  showImg.style.height = "500px";
-  showImg.style.width = "340px";
+  showImg.style.height = "250px";
+  showImg.style.width = "170px";
 
   
-  div.append(showTitle, showNetwork, showSummary, showImg);
-  results.append(div)
+  cardBody.append(showTitle, showNetwork, showSummary, showImg);
+  card.append(cardBody);
+  div.append(card);
+  results.append(div);
 };
 
 //export {makeElements};
